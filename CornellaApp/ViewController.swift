f7370c7801eb3175e6f51ab756f6970ccc5be797
Juan Morillo <juanmorillios@gmail.com>
2017-06-20 22:56:37 +0200

    Initial Commit

---
+++ CornellaApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CornellaApp
+//
+//  Created by Juan Morillo on 20/6/17.
+//  Copyright © 2017 Juan Morillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
