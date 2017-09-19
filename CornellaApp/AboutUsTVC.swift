3ec14de3fb1ea1b54d316ac6f049559e8c1c8643
juanmorillios <juanmorillios@gmail.com>
2017-09-19 17:48:33 +0200

    Add SafariViewController to show social networks.

--- CornellaApp/AboutUsTVC.swift
+++ CornellaApp/AboutUsTVC.swift
@@ -7,34 +7,28 @@
 //
 
 import UIKit
+import SafariServices
 
 class AboutUsTVC: UITableViewController {
   
   //TODO: Falta implementar
   var aboutUS : [AboutUS] = []
-  
-  let sections = ["Dejar valoración", "Siguenos en redes sociales"]
-  let sectionContent = [["Valorar en el App Store", "Déjanos tu feedback"], ["Facebook", "Twitter"]]
-  let sectionLinks = [["https://itunes.apple.com/es/app/fasttaps/id1153034861?mt=8",""],
-                      ["https://facebook.com/juanmorillios", "https://twitter.com/juanmorillios"]]
-  
+  let sections = ["Dejar valoración",
+                  "Siguenos en redes sociales"]
+  let sectionContent = [["Valorar en el App Store",
+                         "Déjanos tu feedback"],
+                        ["Facebook", "Twitter", "Instagram"]]
+  let sectionLinks = [["https://itunes.apple.com/es/app/fasttaps/id1153034861?mt=8",
+                       "http://juanmorillios.com/aboutme/"],
+                      ["https://facebook.com/juanmorillios",
+                       "https://twitter.com/juanmorillios",
+                       "https://www.instagram.com/juanmorillios/"]]
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    
-    
-    
     self.tableView.tableFooterView = UIView(frame: CGRect.zero)
-    
-    
   }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    
-  }
-  
+ 
   // MARK: - Table view data source
   
   override func numberOfSections(in tableView: UITableView) -> Int {
@@ -42,17 +36,12 @@ class AboutUsTVC: UITableViewController {
   }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    
     return self.sectionContent[section].count
-    
   }
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "AboutAsCell", for: indexPath)
-    
     let cellContent = self.sectionContent[indexPath.section][indexPath.row]
-    
     cell.textLabel?.text = cellContent
-    
     return cell
   }
   
@@ -61,76 +50,37 @@ class AboutUsTVC: UITableViewController {
   }
   
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    
     switch indexPath.section {
     case 0:
       switch indexPath.row {
-      case 0:
+    case 0:
         if let appStoreURL = URL(string:self.sectionLinks[0][0])  {
           let app = UIApplication.shared
           if app.canOpenURL(appStoreURL) {
             app.open(appStoreURL, options: [:], completionHandler: nil)
           }
         }
-        
-      case 1:
-        performSegue(withIdentifier: "showWebView", sender: nil)
-        
-        
+    case 1:
+        performSegue(withIdentifier: "showWebView", sender: self.sectionLinks[0][1])
       default:
         break
+      }
+    case 1:
+      if let url = URL(string: self.sectionLinks[1][indexPath.row]) {
+        let safariViewcontroller = SFSafariViewController(url: url)
+        present(safariViewcontroller, animated: true, completion: nil)
       }
     default:
       break
     }
-    
-    
-    
+    self.tableView.deselectRow(at: indexPath, animated: true)
   }
   
-  /*
-   // Override to support conditional editing of the table view.
-   override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-   // Return false if you do not want the specified item to be editable.
-   return true
-   }
-   */
-  
-  /*
-   // Override to support editing the table view.
-   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-   if editingStyle == .delete {
-   // Delete the row from the data source
-   tableView.deleteRows(at: [indexPath], with: .fade)
-   } else if editingStyle == .insert {
-   // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-   }
-   }
-   */
-  
-  /*
-   // Override to support rearranging the table view.
-   override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-   
-   }
-   */
-  
-  /*
-   // Override to support conditional rearranging of the table view.
-   override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-   // Return false if you do not want the item to be re-orderable.
-   return true
-   }
-   */
-  
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destinationViewController.
-   // Pass the selected object to the new view controller.
-   }
-   */
-  
+  // MARK: - Navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "showWebView" {
+      let destinationVC = segue.destination as! WebViewVC
+      destinationVC.urlName = sender as! String
+    }
+  }
 }
